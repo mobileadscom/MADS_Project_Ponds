bf528400125a5c822b95a49c6ef84f41b0a60235
Jeff Paltera <jeffpaltera@outlook.com>
2016-11-24 07:51:37 +0800

    update

---
+++ menoilcontrol-set4/js/main.js
@@ -0,0 +1,288 @@
+/*
+ *
+ * mads - version 2.00.01
+ * Copyright (c) 2015, Ninjoe
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * https://en.wikipedia.org/wiki/MIT_License
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ *
+ */
+var mads = function (options) {
+
+  var _this = this;
+
+  this.render = options.render;
+
+  /* Body Tag */
+  this.bodyTag = document.getElementsByTagName('body')[0];
+
+  /* Head Tag */
+  this.headTag = document.getElementsByTagName('head')[0];
+
+  /* json */
+  if (typeof json == 'undefined' && typeof rma != 'undefined') {
+    this.json = rma.customize.json;
+  } else if (typeof json != 'undefined') {
+    this.json = json;
+  } else {
+    this.json = '';
+  }
+
+  /* fet */
+  if (typeof fet == 'undefined' && typeof rma != 'undefined') {
+    this.fet = rma.customize.fet;
+  } else if (typeof json != 'undefined') {
+    this.fet = fet;
+  } else {
+    this.fet = [];
+  }
+
+  this.fetTracked = false;
+
+  /* load json for assets */
+  this.loadJs(this.json, function () {
+    _this.data = json_data;
+
+    _this.render.render();
+  });
+
+  /* Get Tracker */
+  if (typeof custTracker == 'undefined' && typeof rma != 'undefined') {
+    this.custTracker = rma.customize.custTracker;
+  } else if (typeof custTracker != 'undefined') {
+    this.custTracker = custTracker;
+  } else {
+    this.custTracker = [];
+  }
+
+  /* CT */
+  if (typeof ct == 'undefined' && typeof rma != 'undefined') {
+    this.ct = rma.ct;
+  } else if (typeof ct != 'undefined') {
+    this.ct = ct;
+  } else {
+    this.ct = [];
+  }
+
+  /* CTE */
+  if (typeof cte == 'undefined' && typeof rma != 'undefined') {
+    this.cte = rma.cte;
+  } else if (typeof cte != 'undefined') {
+    this.cte = cte;
+  } else {
+    this.cte = [];
+  }
+
+  /* tags */
+  if (typeof tags == 'undefined' && typeof tags != 'undefined') {
+    this.tags = this.tagsProcess(rma.tags);
+  } else if (typeof tags != 'undefined') {
+    this.tags = this.tagsProcess(tags);
+  } else {
+    this.tags = '';
+  }
+
+  /* Unique ID on each initialise */
+  this.id = this.uniqId();
+
+  /* Tracked tracker */
+  this.tracked = [];
+  /* each engagement type should be track for only once and also the first tracker only */
+  this.trackedEngagementType = [];
+  /* trackers which should not have engagement type */
+  this.engagementTypeExlude = [];
+  /* first engagement */
+  this.firstEngagementTracked = false;
+
+  /* RMA Widget - Content Area */
+  this.contentTag = document.getElementById('rma-widget');
+
+  /* URL Path */
+  this.path = typeof rma != 'undefined' ? rma.customize.src : '';
+
+  /* Solve {2} issues */
+  for (var i = 0; i < this.custTracker.length; i++) {
+    if (this.custTracker[i].indexOf('{2}') != -1) {
+      this.custTracker[i] = this.custTracker[i].replace('{2}', '{{type}}');
+    }
+  }
+};
+
+/* Generate unique ID */
+mads.prototype.uniqId = function () {
+
+  return new Date().getTime();
+}
+
+mads.prototype.tagsProcess = function (tags) {
+
+  var tagsStr = '';
+
+  for (var obj in tags) {
+    if (tags.hasOwnProperty(obj)) {
+      tagsStr += '&' + obj + '=' + tags[obj];
+    }
+  }
+
+  return tagsStr;
+}
+
+/* Link Opner */
+mads.prototype.linkOpener = function (url) {
+
+  if (typeof url != "undefined" && url != "") {
+
+    if (typeof mraid !== 'undefined') {
+      mraid.open(url);
+    } else {
+      window.open(url);
+    }
+  }
+}
+
+/* tracker */
+mads.prototype.tracker = function (tt, type, name, value) {
+
+  /*
+   * name is used to make sure that particular tracker is tracked for only once
+   * there might have the same type in different location, so it will need the name to differentiate them
+   */
+  name = name || type;
+
+  if (tt == 'E' && !this.fetTracked) {
+    for (var i = 0; i < this.fet.length; i++) {
+      var t = document.createElement('img');
+      t.src = this.fet[i];
+
+      t.style.display = 'none';
+      this.bodyTag.appendChild(t);
+    }
+    this.fetTracked = true;
+  }
+
+  if (typeof this.custTracker != 'undefined' && this.custTracker != '' && this.tracked.indexOf(name) == -1) {
+    for (var i = 0; i < this.custTracker.length; i++) {
+      var img = document.createElement('img');
+
+      if (typeof value == 'undefined') {
+        value = '';
+      }
+
+      /* Insert Macro */
+      var src = this.custTracker[i].replace('{{rmatype}}', type);
+      src = src.replace('{{rmavalue}}', value);
+
+      /* Insert TT's macro */
+      if (this.trackedEngagementType.indexOf(tt) != '-1' || this.engagementTypeExlude.indexOf(tt) != '-1') {
+        src = src.replace('tt={{rmatt}}', '');
+      } else {
+        src = src.replace('{{rmatt}}', tt);
+        this.trackedEngagementType.push(tt);
+      }
+
+      /* Append ty for first tracker only */
+      if (!this.firstEngagementTracked && tt == 'E') {
+        src = src + '&ty=E';
+        this.firstEngagementTracked = true;
+      }
+
+      /* */
+      img.src = src + this.tags + '&' + this.id;
+
+      img.style.display = 'none';
+      this.bodyTag.appendChild(img);
+
+      this.tracked.push(name);
+    }
+  }
+};
+
+/* Load JS File */
+mads.prototype.loadJs = function (js, callback) {
+  var script = document.createElement('script');
+  script.src = js;
+
+  if (typeof callback != 'undefined') {
+    script.onload = callback;
+  }
+
+  this.headTag.appendChild(script);
+}
+
+/* Load CSS File */
+mads.prototype.loadCss = function (href) {
+  var link = document.createElement('link');
+  link.href = href;
+  link.setAttribute('type', 'text/css');
+  link.setAttribute('rel', 'stylesheet');
+
+  this.headTag.appendChild(link);
+}
+
+var ponds = function () {
+  /* pass in object for render callback */
+  this.app = new mads({
+    'render': this
+  });
+
+  this.style();
+  this.render();
+  this.events();
+}
+
+ponds.prototype.render = function () {
+  this.app.contentTag.innerHTML = '<div class="container"> \
+  <div id="layer-1" class="layer"> \
+  <img src="' + this.app.path + 'images/ponds-bg.png"/> \
+  <div id="redeem_container"> \
+  <img src="' + this.app.path + 'images/ponds-bttn1.png" id="redeem"/> \
+  <img src="' + this.app.path + 'images/ponds-bttn2.png" id="coupon"/> \
+  <span id="code">123566</span> \
+  </div> \
+  </div> \
+  </div>';
+}
+
+ponds.prototype.style = function () {
+
+  var css = 'body {margin:0;} #rma-widget{width:320px;height:480px;}';
+  css += '.layer{width:320px;height480px;position:relative;}';
+  css += '#redeem_container{width:320px;height:52px;overflow:hidden;position:absolute;bottom:45px;text-align:center;}';
+  css += '#code{position:absolute;color:white;font-size:24px;left:50px;top:10px;letter-spacing:5px;display:none;}';
+  css += '#coupon{display:none;margin:0 auto;}';
+
+  head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+  style.type = 'text/css';
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+
+  head.appendChild(style);
+}
+
+ponds.prototype.events = function () {
+  var _this = this;
+  this.redeem = document.getElementById('redeem');
+  this.coupon = document.getElementById('coupon');
+  this.code = document.getElementById('code');
+
+  this.redeem.addEventListener('click', function (e) {
+    this.app.loadJs('//www.mobileads.com/get_unique_code?userId=2901&campaignId=176&studioId=219&isDemo=1&callback=getCode')
+    e.target.style.display = 'none';
+    _this.coupon.style.display = 'block';
+    _this.code.style.display = 'block';
+  })
+}
+
+function getCode(c) {
+  if (typeof c !== 'undefined' && c.hasOwnProperty('code')) {
+    var code = document.querySelector('#code');
+    code.innerHTML = c.code;
+  }
+}
+
+new ponds();
